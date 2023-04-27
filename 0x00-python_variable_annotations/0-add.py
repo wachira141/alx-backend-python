2621219b98d064d8bee21334ef84a9501d8e2929
wachira140 <wachiraw424@gmail>
2023-04-27 14:42:32 +0300

    2-floor.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -7,4 +7,3 @@ def add(a: float, b: float) -> float:
     '''Adds two floating points and return a float
     '''
     return a + b
-
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''basic annotation floor
+'''Task 2's module.
 '''
 
 
-def floor(n: float) -> int:
-    '''take a float and return its floor
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
     '''
-    return int(n)   
+    return int(a)
